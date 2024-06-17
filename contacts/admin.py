200b3576b8d66d35edde6391447e993128cf4c22
la-machine <frankpythagore@gmail.com>
2024-06-17 18:09:35 +0100

    Djoumessi completed Task 7 (create tests)

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Contact, Group
 
-admin.site.register(Contact)
-admin.site.register(Group)
 admin.site.site_header = "Contact Manager Administration"
 admin.site.site_title = "Contact Manager Admin Portal"
 admin.site.index_title = "Welcome to the Contact Manager Admin"
--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Contact, Group
 
-# Create your tests here.
+class ContactManagerTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.contact = Contact.objects.create(name='Djoumessi Dylan', phone_number='653848259', email='djoumessi@gmail.com', address='odza Yaounde', user=self.user)
+        self.group = Group.objects.create(name='Friends')
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+
+    def test_contact_creation(self):
+        self.assertEqual(self.contact.name, 'Djoumessi Dylan')
+
+    def test_group_creation(self):
+        self.assertEqual(self.group.name, 'Friends')
