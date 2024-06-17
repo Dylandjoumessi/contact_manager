99785bbb912a3fd6117b353768eedcc7d3416485
la-machine <frankpythagore@gmail.com>
2024-06-17 17:51:20 +0100

    Djoumessi completed Task 6 (customize admin site)

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,3 +3,17 @@ from .models import Contact, Group
 
 admin.site.register(Contact)
 admin.site.register(Group)
+admin.site.site_header = "Contact Manager Administration"
+admin.site.site_title = "Contact Manager Admin Portal"
+admin.site.index_title = "Welcome to the Contact Manager Admin"
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'user')
+    search_fields = ('name', 'email', 'phone_number')
+    list_filter = ('user',)
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
